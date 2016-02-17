e5d511b473e3ded1d56939950fe3a32daed2855b
Paulius Labanauskis <paulius.lab@gmail.com>
2016-02-18 00:04:41 +0200

    Changed code organization.

--- src/comment_collapser.js
+++ src/comment_collapser.js
@@ -1,94 +1,46 @@
 // ==UserScript==
 // @name        HackerNews Comment Collapsing
 // @namespace   https://github.com/PauliusLabanauskis
-// @version     0.21
+// @version     0.3
 // @include     http://news.ycombinator.com/*
 // @include     https://news.ycombinator.com/*
 // @grant       none
 // ==/UserScript==
 
-var $hn_collapsing = {
-    createCollapser: function (commentNode, childrenNodes) {
-        var expand = function (e) {
-            var collapserNode = e.target;
-            
-            commentNode.commentTextNode.style.display = "";
-            commentNode.userNode.style.marginBottom = "-10px";
-            
-            childrenNodes.forEach(function(c) {
-                if (c.collapsedBy === commentNode)
-                    c.commentNode.style.display = "";
-            });
-
-            collapserNode.textContent = "[-]";
-            collapserNode.onclick = collapse;
-
-            return false;
-        }
-
-        var collapse = function (e) {
-            var collapserNode = e.target;
-            
-            commentNode.commentTextNode.style.display = "none";
-            commentNode.userNode.style.marginBottom = "10px";
-            
-            childrenNodes.forEach(function(c) {
-                if (c.commentNode.style.display !== "none") {
-                    c.commentNode.style.display = "none";
-                    c.collapsedBy = commentNode;
-                }
-            });
-
-            collapserNode.textContent = "[+]";
-            collapserNode.onclick = expand;
-
-            return false;
-        }
-
-        var collapserAnchor = document.createElement("a");
-        collapserAnchor.class = "collapser";
-        collapserAnchor.onclick = collapse;
-        collapserAnchor.setAttribute("href", "#");
-        collapserAnchor.style.color = "black";
-
-        var anchorText = document.createTextNode("[-]");
-        collapserAnchor.textContent = "[-]";
-
-        return collapserAnchor;
-    },
-
-    addCollapser: function (context, comment, children) {
-        var collapser = context.createCollapser(comment, children);
+(function () {
+    const COLLAPSER_COLLAPSED = '[+]';
+    const COLLAPSER_EXPANDED = '[-]';
+    
+    function addCollapser(comment, children) {
+        var collapser = createCollapser(comment, children);
         
-        var voteArrowNodes = comment.commentNode.getElementsByClassName("votearrow");
+        var voteArrowNodes = comment.commentNode.getElementsByClassName('votearrow');
         if (voteArrowNodes.length > 0)
             var voteAnchorNode =  voteArrowNodes[0].parentNode;
         else
-            var voteAnchorNode = comment.commentNode.querySelector('.votelinks').querySelector('img');
+            voteAnchorNode = comment.commentNode.querySelector('.votelinks').querySelector('img');
         voteAnchorNode.parentNode.insertBefore(collapser, voteAnchorNode);
-    },
+    }
     
-    getComments: function() {
-        var comments = document.getElementsByClassName("athing");
-        comments = Array.prototype.slice.call(comments, 0);
-        comments.splice(0, 1);
-        return comments;
-    },
+    function getComments() {
+        var comments = document.querySelectorAll('.athing');
+        return Array.prototype.slice.call(comments, 1);
+    }
     
-    gatherChildren: function(comments) {
-        var getCurrentNodeInfo = function (commentNode, index) {
-            var userNode = commentNode.querySelector(".comhead").parentNode;
-            var commentTextNode = commentNode.querySelector(".comment");
+    function gatherChildren(comments) {
+        function getCurrentNodeInfo(commentNode, index) {
+            var userNode = commentNode.querySelector('.comhead').parentNode;
+            var commentTextNode = commentNode.querySelector('.comment');
 
-            var indentationNode = commentNode.querySelector(".ind");
-            var indentation = indentationNode.querySelector("img").width;
+            var indentationNode = commentNode.querySelector('.ind');
+            var indentation = indentationNode.querySelector('img').width;
 
             var nodeInfo = {
-                "index": index,
-                "commentNode": commentNode,
-                "userNode": userNode,
-                "commentTextNode": commentTextNode,
-                "indentation": indentation
+                index: index,
+                commentNode: commentNode,
+                userNode: userNode,
+                commentTextNode: commentTextNode,
+                indentation: indentation
             };
             
             return nodeInfo;
@@ -105,8 +57,8 @@ var $hn_collapsing = {
             var commentInfo = getCurrentNodeInfo(comments[i], i);
             
             commentsWithChildren[i] = {
-                "comment": commentInfo,
-                "children": new Array()
+                comment: commentInfo,
+                children: new Array()
             };
             
             while (currentBranch.length > 0 && getLastElementIndentation(currentBranch) >= commentInfo.indentation)
@@ -120,21 +72,67 @@ var $hn_collapsing = {
         }
         
         return commentsWithChildren;
-    },
+    }
 
-    init: function (context) {
-        var comments = context.getComments();
-        comments = context.gatherChildren(comments);
+    function createCollapser(commentNode, childrenNodes) {
+        var expand = function (e) {
+            var collapserNode = e.target;
+            
+            commentNode.commentTextNode.style.display = '';
+            commentNode.userNode.style.marginBottom = '-10px';
+            
+            childrenNodes.forEach(function(c) {
+                if (c.collapsedBy === commentNode)
+                    c.commentNode.style.display = '';
+            });
+
+            collapserNode.textContent = COLLAPSER_EXPANDED;
+            collapserNode.onclick = collapse;
+
+            return false;
+        }
+
+        var collapse = function (e) {
+            var collapserNode = e.target;
+            
+            commentNode.commentTextNode.style.display = 'none';
+            commentNode.userNode.style.marginBottom = '10px';
+            
+            childrenNodes.forEach(function(c) {
+                if (c.commentNode.style.display !== 'none') {
+                    c.commentNode.style.display = 'none';
+                    c.collapsedBy = commentNode;
+                }
+            });
+
+            collapserNode.textContent = COLLAPSER_COLLAPSED;
+            collapserNode.onclick = expand;
+
+            return false;
+        }
+
+        var collapserAnchor = document.createElement('a');
+        collapserAnchor.class = 'collapser';
+        collapserAnchor.onclick = collapse;
+        collapserAnchor.setAttribute('href', '#');
+        collapserAnchor.style.color = 'black';
         
-        for (var key in comments) {
-            context.addCollapser(context, comments[key].comment, comments[key].children);
+        collapserAnchor.textContent = COLLAPSER_EXPANDED;
+
+        return collapserAnchor;
+    }
+    
+    function init () {
+        var comments = getComments();
+        var childComments = gatherChildren(comments);
+        
+        for (var key in childComments) {
+            addCollapser(childComments[key].comment, childComments[key].children);
         }
     }
-};
-
-(function () {
+    
     try {
-        $hn_collapsing.init($hn_collapsing);
+        init();
     }
     catch (e) { console.log(e); }
 })();
