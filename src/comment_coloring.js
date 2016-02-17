97b251c7cc4130db6bb37ded68be79606f6e7f44
Paulius Labanauskis <paulius.lab@gmail.com>
2016-02-17 22:26:27 +0200

    Apply coloring by indentation level.

--- src/comment_coloring.js
+++ src/comment_coloring.js
@@ -1,28 +1,43 @@
 // ==UserScript==
 // @name        HackerNews Comment Coloring
 // @namespace   https://github.com/PauliusLabanauskis
-// @version     0.1
+// @version     0.2
 // @include     http://news.ycombinator.com/*
 // @include     https://news.ycombinator.com/*
 // @grant       none
 // ==/UserScript==
 
-var $hn_coloring = {
-    colors: ["f6f6ef", "fdfdfc"],
-
-    init: function () {
-        var elements = document.getElementsByClassName("default");
-
-        for (var i = 0; i < elements.length; i++) {
-            var color = this.colors[i % 2];
-            elements[i].style.backgroundColor = color;
-        }
-    }
-};
-
 (function () {
+    'use strict';
+    
+    const HIGHLIGHT_COLOR = 'fdfdfc';
+    const BASE_INDENT_PX = 40;
+    
+    function getCommentIndentations() {
+        var things = Array.prototype.slice.call(document.getElementsByClassName('athing'), 1);
+        var indentations = Array.prototype.map.call(things, function(currentValue) {
+            var indentation = currentValue.querySelector('.ind > img').width / BASE_INDENT_PX;
+            return {
+                indentation: indentation,
+                comment: currentValue
+            }
+        });
+        return indentations;
+    }
+    
+    function init() {
+        var comments = getCommentIndentations();
+        var coloredComments = comments.filter(function(value) {
+            return value.indentation % 2 != 0;
+        });
+        coloredComments.forEach(function(value) {
+            var commentNode = value.comment.querySelector('.default');
+            commentNode.style.backgroundColor = HIGHLIGHT_COLOR;
+        });
+    }
+    
     try {
-        $hn_coloring.init();
+        init();
     }
     catch (e) { console.log(e); }
 })();
