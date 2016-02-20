cd34c4dcd15117265c43e3b8d6bff96a699a5d1a
Paulius Labanauskis <paulius.lab@gmail.com>
2016-02-20 10:42:04 +0200

    Collapsing of comments with text that was not found.

--- src/comment_filter.js
+++ src/comment_filter.js
@@ -11,9 +11,23 @@
     'use strict';
     
     function Filter(currentDocument) {
-        var prototype = Object.getPrototypeOf(this);
+        this.currentDocument = currentDocument;
+        this.discoverNonMatchingComments = function(text) {
+            var filtered = this.currentDocument.comments.filter(
+                function(comment) {
+                    var commentText = comment.getText();
+                    return commentText == null ?
+                        false : comment.getText().indexOf(text) === -1;
+                });
+            return filtered;
+        };
         
-        prototype.onInput = (value) => console.log(value);
+        var prototype = Object.getPrototypeOf(this);        
+        var onInput = function(value) {
+            var nonMatchingComments = this.discoverNonMatchingComments(value);
+            nonMatchingComments.forEach(function(comment) { comment.collapse() });
+        }.bind(this);
+        prototype.onInput = onInput;
     }
     
     function FilterInput(onInputCallback) {
@@ -21,17 +35,34 @@
             var input = document.createElement('input');
             input.type = 'text';
             input.style.marginRight = '10px';
-            input.addEventListener('input', ev => onInputCallback(input.value));
+            input.addEventListener('input', function(ev) { onInputCallback(input.value) });
             return input;
         }
         
         this.inputField = createInputField();
         
         var prototype = Object.getPrototypeOf(this);
-        prototype.getTextField = () => this.inputField;
+        prototype.getTextField = function() { return this.inputField };
+    }
+    
+    function DOMComment(commentNodeDOM) {
+        this.commentNodeDOM = commentNodeDOM;
+        
+        var prototype = Object.getPrototypeOf(this);
+        prototype.collapse = function() { this.commentNodeDOM.style.display = 'none'; }
+        prototype.expand = function() { this.commentNodeDOM.style.display = '' };
+        prototype.getText = function() {
+            var commentTextNode = this.commentNodeDOM.querySelector('.comment span');
+            return commentTextNode == null ? null : commentTextNode.innerHTML;
+        }
     }
     
     function CurrentDocument() {
+        var commentNodes = document.querySelectorAll('.comment-tree .athing');
+        this.comments = [];
+        Array.prototype.forEach.call(commentNodes,
+            function(node) { this.comments.push(new DOMComment(node)) }, this);
+        
         var prototype = Object.getPrototypeOf(this);
         prototype.placeInputField = function(input) {
             var placement = document.querySelectorAll('td > .pagetop')[1];
@@ -40,9 +71,13 @@
         }
     }
     
-    var currentDocument = new CurrentDocument();
-    var filter = new Filter(currentDocument);
-    var filterInput = new FilterInput(filter.onInput);
-    
-    currentDocument.placeInputField(filterInput.getTextField());
+    try {
+        var currentDocument = new CurrentDocument();
+        var filter = new Filter(currentDocument);
+        var filterInput = new FilterInput(filter.onInput);
+        
+        currentDocument.placeInputField(filterInput.getTextField());
+    } catch (e) {
+        console.log(e);
+    }
 })();
